733965de171cd16965d554fec128526c2d6c0aab
redeye32 <andrewmorrison1289@gmail.com>
2025-08-01 11:47:54 -0500

    10 solved

--- exercises/e10.js
+++ exercises/e10.js
@@ -1,17 +1,25 @@
 export const getFirstResolvedPromise = (promises) => {
   //*  write code to pass test ⬇ ️
+  return Promise.any(promises);
 };
 
 export const getFirstPromiseOrFail = (promises) => {
   //*  write code to pass test ⬇ ️
+  return Promise.race(promises);
 };
 
 export const getQuantityOfRejectedPromises = (promises) => {
   //*  write code to pass test ⬇ ️
+  return Promise.allSettled(promises).then(
+    (results) => results.filter((res) => res.status === "rejected").length
+  );
 };
 
 export const getQuantityOfFulfilledPromises = (promises) => {
   //*  write code to pass test ⬇ ️
+  return Promise.allSettled(promises).then(
+    (results) => results.filter((res) => res.status === "fulfilled").length
+  );
 };
 
 //!  ⬇ ⬇ ⬇ ⬇ Don't Edit This Array ⬇ ⬇ ⬇ ⬇
@@ -45,4 +53,9 @@ export const fetchAllCharactersByIds = async (ids) => {
   // To solve this you must fetch all characters passed in the array at the same time
   // use the `fetchCharacterById` function above to make this work
   //*  write code to pass test ⬇ ️
+  const characterPromises = ids.map((id) => fetchCharacterById(id));
+  const characters = await Promise.allSettled(characterPromises);
+  const missingChars = characters.some((char) => char.status === "rejected");
+  if (missingChars) return [];
+  return characters.map((char) => char.value);
 };
